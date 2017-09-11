37d3de5f3e92631209cb10311ec74a09b195ea6f
IrvinX <171427088@qq.com>
2017-09-11 16:38:38 +0800

    user-center gateway

--- irvin-gateway/src/main/java/irvin/gateway/GatewayApplication.java
+++ irvin-gateway/src/main/java/irvin/gateway/GatewayApplication.java
@@ -1,9 +1,7 @@
-package top.toptimus;
+package irvin.gateway;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
