f7bdf23783e10a08eb2abbe68a3f9c262942bfb4
IrvinX <171427088@qq.com>
2017-09-11 14:22:17 +0800

    swagger

---
+++ irvin-uc/src/main/java/irvin/uc/common/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package irvin.uc.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author irvin
+ * @date Create in 下午13:30 2017/9/11
+ * @description Swagger config
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()  // 选择那些路径和api会生成document
+				.apis(RequestHandlerSelectors.any()) // 对所有api进行监控
+				.paths(PathSelectors.any()) // 对所有路径进行监控
+				.build();
+	}
+
+}
